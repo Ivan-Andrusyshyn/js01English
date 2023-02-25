3bec7ee375ee310595a6f68550dae7c8d0082111
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-02-25 15:35:17 +0100

    23

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
-const arr =[
-     'time ', 'information', 'people', 'thing ', 'community ', 
-     'man ', 'woman ', 'way ',
- 'life', 'child ','world','school', 'state ', 'family',
- 'student','group ','country ','problem ',
- 'hand ','part ','place ','case ','which','like','been',
-  'in', 'or',
- 'she', 'him', 'call', 'is','one','do','into','who',
- 'you', 'had', 'how', 'time','oil', 'that', 'by',
- 'their','has', 'its', 'it', 'word', 'if', 
- 'look', 'now', 'he', 'but', 'will', 'two', 'find', 
- 'was', 'not', 'up', 'more', 'long', 'for', 'what', 
- 'other', 'write', 'down', 'on', 'all', 'about',
-  'go', 'day', 'are', 'were', 'out', 'see', 'did', 
-  'as', 'we', 'many', 'number', 'get', 'with', 'when', 'then', 'no', 
-  'come', 'his', 'your', 'them', 'way', 'made', 'they', 'can', 'these', 
-  'could', 'may', 'I', 'said', 'so', 'people', 'part']
+// const arr =[
+//      'time ', 'information', 'people', 'thing ', 'community ', 
+//      'man ', 'woman ', 'way ',
+//  'life', 'child ','world','school', 'state ', 'family',
+//  'student','group ','country ','problem ',
+//  'hand ','part ','place ','case ','which','like','been',
+//   'in', 'or',
+//  'she', 'him', 'call', 'is','one','do','into','who',
+//  'you', 'had', 'how', 'time','oil', 'that', 'by',
+//  'their','has', 'its', 'it', 'word', 'if', 
+//  'look', 'now', 'he', 'but', 'will', 'two', 'find', 
+//  'was', 'not', 'up', 'more', 'long', 'for', 'what', 
+//  'other', 'write', 'down', 'on', 'all', 'about',
+//   'go', 'day', 'are', 'were', 'out', 'see', 'did', 
+//   'as', 'we', 'many', 'number', 'get', 'with', 'when', 'then', 'no', 
+//   'come', 'his', 'your', 'them', 'way', 'made', 'they', 'can', 'these', 
+//   'could', 'may', 'I', 'said', 'so', 'people', 'part']
 
-  const arr2 =['час', 'інформація', 'люди', 'річ ', 'громада', 
-  'чоловік ', 'жінка ', 'шлях ',
-'життя', 'дитина ','світ','школа', 'держава ', "сім'я",
-'студент','група ','країна ','проблема ',
-'рука ','частина ','місце ','завдання ','котрий','подобатися','бути',
-'в', 'або',
-'вона', 'він', 'дзвінок', 'бути','один','робити','в чомусь','хто',
-'ти', 'мав', 'як', 'час','олія', 'той,котрий', 'за',
-'їх','маю', 'це', 'серeдній рід', 'слово', 'якщо', 
-'подивіться', 'зараз', 'він', 'але', 'буде', 'два', 'знайти', 
-'був', 'не', 'вгору', 'більше', 'довго', 'для', 'що', 
-'інший', 'писати', 'вниз', 'на', 'всі', 'приблизно',
-'іти', 'день', 'є', 'були', 'поза (чимось)', 'бачити', 'зробив', 
-'як', 'ми', 'багато', 'номер', 'отримати', 'з', 'коли', 'потім', 'ні', 
-'прийти', 'його', 'твої', 'їх', 'шлях', 'зроблемно', 'вони', 'могти', 'ці', 
-'міг би', 'можливо', 'Я', 'сказав', 'так', 'люди', 'частина']
+//   const arr2 =['час', 'інформація', 'люди', 'річ ', 'громада', 
+//   'чоловік ', 'жінка ', 'шлях ',
+// 'життя', 'дитина ','світ','школа', 'держава ', "сім'я",
+// 'студент','група ','країна ','проблема ',
+// 'рука ','частина ','місце ','завдання ','котрий','подобатися','бути',
+// 'в', 'або',
+// 'вона', 'він', 'дзвінок', 'бути','один','робити','в чомусь','хто',
+// 'ти', 'мав', 'як', 'час','олія', 'той,котрий', 'за',
+// 'їх','маю', 'це', 'серeдній рід', 'слово', 'якщо', 
+// 'подивіться', 'зараз', 'він', 'але', 'буде', 'два', 'знайти', 
+// 'був', 'не', 'вгору', 'більше', 'довго', 'для', 'що', 
+// 'інший', 'писати', 'вниз', 'на', 'всі', 'приблизно',
+// 'іти', 'день', 'є', 'були', 'поза (чимось)', 'бачити', 'зробив', 
+// 'як', 'ми', 'багато', 'номер', 'отримати', 'з', 'коли', 'потім', 'ні', 
+// 'прийти', 'його', 'твої', 'їх', 'шлях', 'зроблемно', 'вони', 'могти', 'ці', 
+// 'міг би', 'можливо', 'Я', 'сказав', 'так', 'люди', 'частина']
 
+const arr = [
+{word:'life',translite:'життя'},{word:'world', translite:'світ'},{word: 'case', translite: 'завдання'},
+{word:'time', translite:'час'},{word:'by', translite:'за'},{word:'hand', translite:'рука'},
+{word:'information', translite:'інформація'},{word:'been', translite:'був'},{word:'or', translite:'або'},
+{word:'school', translite:'школа'},{word:'family', translite:"сім'я"},{word:'in', translite:'в'},
+{word:'community', translite:'спільнота'},{word:'people', translite:'люди'},{word:'thing', translite:'річ'},
+{word:'problem', translite:'проблема'},{word:'country', translite:'країна'},{word:'place', translite:'місце'},
+{word:'you', translite:'ти'},{word:'had', translite:'мав'},{word:'do', translite:'робити'},
+{word:'call', translite:'дзвонити'},{word:'one', translite:'1'},{word:'who', translite:'хто'},
+{word:'into', translite:'в'},{word:'time', translite:'час'},{word:'their', translite:'їх'},
+{word:'how', translite:'як'},{word:'that', translite:'що'},{word:'has', translite:'має'},
+{word:'its', translite:'його'},{word:'it', translite:'воно'},{word:'word', translite:'слово'},
+{word:'if', translite:'якщо'},{word:'look', translite:'дивитись, вигляд'},{word:'now', translite:'зараз'},
+{word:'he', translite:'він'},{word:'but', translite:'але'},{word:'will', translite:'буду'},
+{word:'two', translite:'2'},{word:'all', translite:'всі'},{word:'about', translite:'навколо'},
+{word:'go', translite:'іти'},{word:'day', translite:'день'},{word:'are', translite:'є'},
+{word:'did', translite:'зробив'},{word:'out', translite:'зовні'},{word:'see', translite:'бачити'},
+{word:'as', translite:'як'},{word:'we', translite:'вони'},{word:'many', translite:'багато'},
+{word:'number', translite:'номер'},{word:'get', translite:'отримати'},{word:'with', translite:'з'},
+{word:'when', translite:'коли'},{word:'then', translite:'потім'},{word:'no', translite:'ні'},
+{word:'come', translite:'прийти'},{word:'his', translite:'його'},{word:'your', translite:'твій'},
+{word:'them', translite:'їх'},{word:'way', translite:'шлях'},{word:'made', translite:'зроблено'},
+{word:'they', translite:'вони'},{word:'can', translite:'могти'},{word:'these', translite:'ці'},
+{word:'could', translite:'міг'},{word:'may', translite:'може'},{word:'I', translite:'я'},
+{word:'said', translite:'сказав'},{word:'so', translite:'так'},{word:'people', translite:'люди'},
+{word:'part', translite:'частина'},{word:'car', translite:'машина'},{word:'tomorrow', translite:'завтра'},
+{word:'yesterday', translite:'вчора'},{word:'month', translite:'місяць'},{word:'week', translite:'тиждень'},
+{word:'year', translite:'рік'},{word:'hundred', translite:'100'},{word:'ten', translite:'10'},
+{word:'five', translite:'5'},{word:'six', translite:'6'},{word:'made', translite:'зроблено'},
+{word:'sun', translite:'сонце'},{word:'moon', translite:'місяць'},{word:'night', translite:'ніч'}
+]
 
   const englishWord = document.querySelector('.english-word')
   const btnStart = document.querySelector('.btn-start')
@@ -44,9 +75,19 @@ const arr =[
     const night = document.querySelector('.eng')
    const text = document.querySelector('.text')
    const sectionNight = document.querySelector('.main')
+   englishWord.textContent = 'Start';
     
 
-console.log(arr[0])
+
+  const word = [];
+ const  tran = [];
+   for (let w of arr) {
+     word.push(w.word);
+   }
+   for (let w of arr) {
+    tran.push(w.translite);
+  }
+  
   let count = -1;
 
   btnStop.addEventListener(
@@ -54,8 +95,8 @@ console.log(arr[0])
                    
                         () => { 
                             count++
-                            englishWord.textContent = arr[count]
                             screenSecond.classList.remove('space-start')
+                            englishWord.textContent = word[count]
 
                             if(count==arr.length){
                                count=-1
@@ -68,25 +109,32 @@ console.log(arr[0])
                     () => { 
                         count--
                         screenSecond.classList.remove('space-start')
-                       englishWord.textContent = arr[count]
-                        if(count<=-1){
+                       englishWord.textContent = word[count]
+
+                        if(count<=0){
                             count=arr.length
                          }
 
              })
-             screenFirst.addEventListener("click",()=>{
-               screenSecond.classList.add('space-start')
-               translite.textContent = arr2[count]
+              
 
+             screenFirst.addEventListener("click",()=>{
+              translite.textContent = tran[count]
+                    if(count>=0){
+                      screenSecond.classList.add('space-start')
+
+                    }
              } )
+             
+             
+             
              screenSecond.addEventListener("click",()=>{
                 screenSecond.classList.remove('space-start')
               } )
 
 
               night.addEventListener(
-                "click",
-           
+                "click",   
                 () => { 
                   btnStart.classList.toggle('btn-night')
          btnStop.classList.toggle('btn-night')
